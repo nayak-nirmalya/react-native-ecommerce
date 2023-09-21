da399a965d0a87fb21396537913e3ea9c5e84933
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-21 13:37:16 +0530

    added schema for products table with drizzle

--- express-server/src/db/schema.ts
+++ express-server/src/db/schema.ts
@@ -1,5 +1,22 @@
-export const products = {};
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  doublePrecision,
+  integer,
+} from "drizzle-orm/pg-core";
 
-export const orders = {};
+export const products = pgTable("products", {
+  id: serial("id").primaryKey(),
+  product_name: varchar("product_name", { length: 100 }).notNull(),
+  product_category: varchar("product_category", { length: 100 }).notNull(),
+  product_description: text("product_description").notNull(),
+  product_price: doublePrecision("product_price").notNull(),
+  product_stock: integer("product_stock").notNull(),
+  product_image: text("product_image").notNull(),
+});
 
-export const order_items = {};
+// export const orders = {};
+
+// export const order_items = {};
