140e6e8c2063253e941e511b505c011bbaee5b28
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-23 12:52:15 +0530

    migrate to neon db

--- express-server/migrate.ts
+++ express-server/migrate.ts
@@ -4,22 +4,24 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 if (process.env.NODE_ENV === "production") {
-  console.log("Running in production mode.");
+  console.log("Running in PRODUCTION_MODE.");
   config({ path: ".prod.env" });
 } else {
-  console.log("Running in development mode.");
+  console.log("Running in DEVELOPMENT_MODE.");
   config({ path: ".dev.env" });
 }
+
 const { DB_URL } = process.env;
 const databaseUrl = drizzle(postgres(DB_URL!, { ssl: "require", max: 1 }));
 
 const main = async () => {
   try {
     await migrate(databaseUrl, { migrationsFolder: "drizzle" });
-    console.log("Migration complete");
+    console.log("Migration Complete!");
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
   process.exit(0);
 };
+
 main();
