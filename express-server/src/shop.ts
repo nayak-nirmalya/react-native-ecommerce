fff4a3fdfb7de6e1e5470441081ebc4e20a032b5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-23 13:30:16 +0530

    added handleQueryError fn

--- express-server/src/shop.ts
+++ express-server/src/shop.ts
@@ -2,6 +2,22 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
+
 import { OrderItem, order_items, orders, products } from "@/src/db/schema";
 
-export const router = express.Router();
+const router = express.Router();
+
+const pool = new Pool({
+  connectionString: `${process.env.DB_URL}`,
+  ssl: { rejectUnauthorized: false },
+});
+const db = drizzle(pool);
+
+const handleQueryError = (err: any, res: Response) => {
+  console.error("Error Executing Query: ", err);
+  res
+    .status(500)
+    .json({ error: "An error occurred while executing the query." });
+};
+
+export { router };
