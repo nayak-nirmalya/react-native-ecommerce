deb6e0a49c724e12277f8637939bbe7fc77e47cb
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-23 13:47:30 +0530

    bug fix: added dotenv/config to read env vars

--- express-server/src/shop.ts
+++ express-server/src/shop.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
+import "dotenv/config";
 
 import { OrderItem, order_items, orders, products } from "@/src/db/schema";
 
