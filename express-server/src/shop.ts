386aa7c25c34f92ca5c248113901f838ef031b10
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-24 12:48:24 +0530

    added route POST: /orders to add new order and item

--- express-server/src/shop.ts
+++ express-server/src/shop.ts
@@ -49,4 +49,64 @@ router.get("/products/:id", async (req: Request, res: Response) => {
   }
 });
 
+router.post("/orders", async (req: Request, res: Response) => {
+  try {
+    const { email, products: orderBody } = req.body;
+
+    const order = await db.transaction(async (trx) => {
+      const [newOrder] = await trx
+        .insert(orders)
+        .values({ customer_email: email })
+        .returning();
+
+      const productPrices = await Promise.all(
+        orderBody.map(async (orderItem: any) => {
+          const [res] = await db
+            .select()
+            .from(products)
+            .where(eq(products.id, +orderItem.product_id));
+
+          return res.product_price;
+        })
+      );
+
+      const orderProducts = await Promise.all(
+        orderBody.map(async (orderItem: any, index: number) => {
+          const total = (+productPrices[index] * +orderItem.quantity).toFixed(
+            2
+          );
+
+          const [orderProduct] = await trx
+            .insert(order_items)
+            .values({
+              order_id: newOrder.id,
+              product_id: orderItem.product_id,
+              quantity: orderItem.quantity,
+              total: +total,
+            })
+            .returning();
+
+          return orderProduct;
+        })
+      );
+
+      const total = orderProducts.reduce((acc: number, curr: OrderItem) => {
+        return acc + curr.total;
+      }, 0);
+
+      const [updatedOrder] = await trx
+        .update(orders)
+        .set({ total: total.toFixed(2) })
+        .where(eq(orders.id, newOrder.id))
+        .returning();
+
+      return { ...updatedOrder, products: orderProducts };
+    });
+
+    res.json(order);
+  } catch (err) {
+    handleQueryError(err, res);
+  }
+});
+
 export { router };
