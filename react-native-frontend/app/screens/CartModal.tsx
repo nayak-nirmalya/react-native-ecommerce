19df16a3f328f7f12b9650ead08c8dee2e0eafcf
Nirmalya Nayak <n.nayak263@gmail.com>
2023-09-28 12:26:17 +0530

    added modal CartModal.tsx

---
+++ react-native-frontend/app/screens/CartModal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const CartModal = () => {
+  return (
+    <View>
+      <Text>CartModal</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
